7f9502c195c7b0a88bcd7f2fcb25e17b65a9ac66
NicoGuerrero11 <nicoguerrero0800@gmail.com>
2025-06-24 12:23:27 -0600

    usamos componente card en app para mostrar las tarjetas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,49 @@
 import React from 'react';
+import Card from './components/Card.jsx';
+
+const persons = [
+    {
+        name: "nicolas",
+        image: "nicolas.png",
+        description: "web development"
+    },
+    {
+        name: "gabriela",
+        image: "gabriela.png",
+        description: "student"
+    },
+    {
+        name: "Agustina",
+        image: "agus.png",
+        description: "student"
+    },
+    {
+        name: "Marcelo",
+        image: "marce.png",
+        description: "business"
+    },
+    {
+        name: "Maritza",
+        image: "mar.png",
+        description: "loving god"
+    },
+
+];
 
 const App = () => {
-    return (
-        <div>
-           <h1>Hello world</h1>
-        </div>
-    );
+    return(
+        <>
+            <h1>Presentation Card</h1>
+            {persons.map((person, index) => (
+                <Card
+                    key ={index}
+                    name={person.name}
+                    image={person.image}
+                    description={person.description}
+                />
+            ))}
+        </>
+    )
 };
 
 export default App;
